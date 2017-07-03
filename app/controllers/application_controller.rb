5b47f051bea00feec7215ec222b6e82e9fdffc12
sephvice <johnjoseph.vicente@adish.co.jp>
2017-07-03 09:27:36 +0800

    source

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+class ApplicationController < ActionController::Base
+  before_action :authorize
+  protect_from_forgery with: :exception
+    before_action :set_il8n_locale_from_params
+  
+  protected
+  
+  def set_il8n_locale_from_params
+    if params[:locale]
+	  if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+    else
+	  flash.now[:notice] =
+	    "#{params[:locale]} translation not available"
+		logger.error flash.now[:notice]
+		end
+	end
+  end
+	
+  def default_url options
+    { locale: I18n.locale }
+  end
+	
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  
+  get 'admin/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
+  
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+  
+  get 'store/index'
+
+  resources :products do
+    get :who_bought, on: :member
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+    root 'store#index', as: 'store'  
+  end
+end
